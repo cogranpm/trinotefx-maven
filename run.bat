4e4da15e41c82eab784f307b9ed470f01de62bda
Paul Martin <cogranpm@gmail.com>
2020-09-25 17:23:43 -0500

    more stuff

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-mvn javafx:run
+# mvn javafx:run
+java --module-path "C:\lib\java\javafx-sdk-11.0.2\lib" --add-modules=javafx.controls,javafx.fxml -jar .\target\hellofx-1.0-SNAPSHOT-jar-with-dependencies.jar
